5da707b2d5f8203ad0bbd0037a874e06fe35e43e
Bartłomiej Kręgielewski <bartlomiejkregielewski@gmail.com>
2020-08-04 15:24:23 +0200

    Persisting changes, loading indicator

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -8,6 +8,7 @@ interface IProps {
 	activity: IActivity;
 	createActivity: (activity: IActivity) => void;
 	editActivity: (activity: IActivity) => void;
+	submitting: boolean;
 }
 
 export const ActivityForm: React.FC<IProps> = ({
@@ -15,6 +16,7 @@ export const ActivityForm: React.FC<IProps> = ({
 	activity: initialFormState,
 	createActivity,
 	editActivity,
+	submitting,
 }) => {
 	const initializeForm = () => {
 		if (initialFormState) {
@@ -99,6 +101,7 @@ export const ActivityForm: React.FC<IProps> = ({
 					positive
 					type="submit"
 					content="Submit"
+					loading={submitting}
 				/>
 				<Button
 					floated="right"
