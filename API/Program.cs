5eb060fa119a4aa5e06bebc7501bfa79f6f2c7bc
Bartłomiej Kręgielewski <bartlomiejkregielewski@gmail.com>
2020-09-24 01:57:58 +0200

    Added security policies

--- API/Program.cs
+++ API/Program.cs
@@ -38,6 +38,7 @@ namespace API
 			Host.CreateDefaultBuilder(args)
 				.ConfigureWebHostDefaults(webBuilder =>
 				{
+					webBuilder.UseKestrel(x => x.AddServerHeader = false);
 					webBuilder.UseStartup<Startup>();
 				});
 	}
